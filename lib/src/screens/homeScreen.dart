a215488ddcd50484dce079e5342c9ff84d94a460
Fellipe Malta <fellipe.dm@hotmail.com>
2018-09-27 21:54:55 -0300

    removed the animation

--- lib/src/screens/homeScreen.dart
+++ lib/src/screens/homeScreen.dart
@@ -9,48 +9,17 @@ class HomeScreen extends StatefulWidget {
   }
 }
 
-class HomeScreenState extends State<HomeScreen>
-    with SingleTickerProviderStateMixin {
-  Animation _opaqueAnimation;
-  AnimationController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = new AnimationController(
-      vsync: this,
-      duration: new Duration(seconds: 1),
-    );
-    _controller.forward();
-
-    _opaqueAnimation = new Tween(
-      begin: 0.0,
-      end: 1.0,
-    ).animate(new CurvedAnimation(
-        curve: new Interval(0.250, 0.750), parent: _controller));
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _controller.dispose();
-  }
-
+class HomeScreenState extends State<HomeScreen> {
   showOverlay(BuildContext context) async {
     OverlayState overlayState = Overlay.of(context);
     OverlayEntry overlayEntry = OverlayEntry(
-      builder: (context) => new Opacity(
-            opacity: _opaqueAnimation.value,
-            child: new Stack(children: [
-              new Positioned(
-                top: 30.0,
-                right: 10.0,
-                child: new CircleAvatar(
-                  radius: 10.0,
-                  child: new Text('1'),
-                ),
-              ),
-            ]),
+      builder: (context) => new Positioned(
+            top: 30.0,
+            right: 10.0,
+            child: new CircleAvatar(
+              radius: 10.0,
+              child: new Text('1'),
+            ),
           ),
     );
 
